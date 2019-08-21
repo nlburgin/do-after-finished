65fc0b2b4ceb12749845711aa0f2cb5a1ee76b62
nlburgin <30382228+nlburgin@users.noreply.github.com>
2019-08-21 15:13:58 -0400

    remove debug echoes

--- alarmclock.sh
+++ alarmclock.sh
@@ -5,13 +5,6 @@ test_positive() { return `echo "$1 < 0" | bc`; }
 now=`date +%s`
 then=`date -d "$1" +%s`
 diff=`echo "$then - $now" | bc`
-echo $now
-echo $then
-echo "$then - $now"
-echo $diff
-
-test_positive $diff
-echo $?
 
 if test_positive $diff
 then
